b79f030d76347dd807f1b54e108ba46420560322
John Clark <socrates37@gmail.com>
2017-02-28 09:12:43 -0700

    added package-manager to defaults; fixed file bug; closes #1

--- app.js
+++ app.js
@@ -1,14 +1,21 @@
 const {app, BrowserWindow} = require('electron');
+const fs = require('fs');
 const path = require('path');
 const server = require('./server.js');
 
 app.commandLine.appendSwitch('js-flags', '--harmony-async-await');
 
-// TODO ensure these directories exist!
 process.env.TH_PATH = path.join(app.getPath('home'), '.treehub');
+if (!fs.existsSync(process.env.TH_PATH)) {
+  fs.mkdirSync(process.env.TH_PATH);
+}
 process.env.TH_PACKAGE_PATH = path.join(process.env.TH_PATH, 'packages');
+if (!fs.existsSync(process.env.TH_PACKAGE_PATH)) {
+  fs.mkdirSync(process.env.TH_PACKAGE_PATH);
+}
 
-const requiredPackages = ['app'];
+
+const requiredPackages = ['app', 'package-manager'];
 let mainWindow = null;
 
 // Ensure only 1 app runs
