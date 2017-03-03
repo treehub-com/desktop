a60033b763ef976d5bb68b7bac3c1246cada918a
John Clark <socrates37@gmail.com>
2017-03-03 15:13:14 -0700

    fixed formatting

--- app.js
+++ app.js
@@ -31,19 +31,19 @@ app.on('window-all-closed', () => {
 });
 
 autoUpdater.on('update-downloaded', (event, info) => {
-		// Ask user to update the app
-		dialog.showMessageBox({
-			type: 'question',
-			buttons: ['Install and Relaunch', 'Later'],
-			defaultId: 0,
-			message: 'A new version of Treehub has been downloaded',
-			detail: 'It will be installed the next time you restart the application',
-		}, (response) => {
-			if (response === 0) {
-				setTimeout(() => autoUpdater.quitAndInstall(), 1);
-			}
-		});
-	});
+  // Ask user to update the app
+  dialog.showMessageBox({
+    type: 'question',
+    buttons: ['Install and Relaunch', 'Later'],
+    defaultId: 0,
+    message: 'A new version of Treehub has been downloaded',
+    detail: 'It will be installed the next time you restart the application',
+  }, (response) => {
+    if (response === 0) {
+      setTimeout(() => autoUpdater.quitAndInstall(), 1);
+    }
+  });
+});
 
 // TODO show loading screen and any starup errors
 app.on('ready', async () => {
